356abd8f13650c8af258c8f522a8a0d5ea8a4ff5
siliconprime-baotran <bao.tran@siliconprime.com>
2025-05-04 01:58:56 +0700

    [Chore] Add supense loading for lazyload component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,30 @@
+import { Spin } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+
+export default function Loading({
+  blurBackground = true,
+  fullScreen
+}: {
+  blurBackground?: boolean // optional, default to false
+  fullScreen?: boolean // optional, default to false
+}) {
+  return (
+    <>
+      {blurBackground ? (
+        <div className="bg-white/30 backdrop-saturate-50 absolute left-0 top-0 w-full h-full z-10">
+          <Spin
+            className="absolute left-[50%] top-[50%]"
+            fullscreen={fullScreen}
+            indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
+          ></Spin>
+        </div>
+      ) : (
+        <Spin
+          className="absolute left-[50%] top-[50%]"
+          fullscreen={fullScreen}
+          indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
+        ></Spin>
+      )}
+    </>
+  )
+}
--- src/layout/OneColumnsLayout.tsx
+++ src/layout/OneColumnsLayout.tsx
@@ -1,24 +1,22 @@
 'use client'
-import { useEffect } from "react";
-import { useState } from "react";
+
+import Loading from "@/components/Loading/Loading";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
+import { Suspense } from "react";
 export default function OneColumnsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
-      {isLoading && <div className="flex items-center justify-center h-screen">Loading...</div>}
       <Header />
       <main className={`flex-1 bg-[url(/assets/images/body-background.png)] bg-center bg-no-repeat bg-cover`}>
-        {children}
+        <Suspense fallback={<Loading fullScreen />}>
+          {children}
+        </Suspense>
       </main>
       <Footer />
     </div>
